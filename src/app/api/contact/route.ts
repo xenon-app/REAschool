ec8ccd539b6cf2b85bf19a0fc117f8ded42a7bd8
xenon-app <divyanshucmd@gmail.com>
2026-03-15 14:45:16 +0000

    Contact us

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -10,8 +10,8 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.REA,
+        pass: process.env.uilt ztkk dsem xwky,
       },
     });
 
